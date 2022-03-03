e56fae11ccd7f3337a4406ae11df2370a7d804fe
Zach <zawood11@gmail.com>
2022-03-03 01:11:24 -0500

    commit tree 662c6e899b87abd8e03cd7528f2b6546f47bc164

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,65 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness
+        @happiness.clamp(0,10)
+    end
+
+    def hygiene
+        @hygiene.clamp(0,10)
+    end
+
+    def clean?
+        hygiene > 7
+    end
+    
+    def happy?
+        happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        when 'weather'
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
